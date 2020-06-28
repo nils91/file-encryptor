edb8393c34a2137682b5f5eb1e774aa898b7513b
Nils Dralle <nils.91@googlemail.com>
2020-06-28 16:37:31 +0200

    simple base64 string tests

--- src/de/dralle/passwordmanager/test/Base64Tests.java
+++ src/de/dralle/passwordmanager/test/Base64Tests.java
@@ -18,12 +18,24 @@ import org.junit.jupiter.api.Test;
 class Base64Tests {
 
 	@Test
-	void test(){
-	Encoder encoder = Base64.getEncoder();
-	Decoder decoder = Base64.getDecoder();
-byte[] inputBytes = new byte[] {0,1,2,3,4,5,6,7,8,9};
-byte[] encodedBytes = encoder.encode(inputBytes);
-byte[] decodedBytes = decoder.decode(encodedBytes);assertArrayEquals(inputBytes, decodedBytes);
+	void testByteEncodingDecoding() {
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		assertArrayEquals(inputBytes, decodedBytes);
+	}
+	@Test
+	void testSimpleStringEncodingDecoding() {
+		String input="Hello World!";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes();
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String encoded=new String(decodedBytes);
+		assertEquals(input, encoded);
 	}
 
 }
