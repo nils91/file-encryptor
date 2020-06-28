175265148ab527cb6d734063dca1aa764c605361
Nils Dralle <nils.91@googlemail.com>
2020-06-28 19:28:09 +0200

    swap testfiles

--- src/de/dralle/passwordmanager/test/BasicBase64Tests.java
+++ src/de/dralle/passwordmanager/test/BasicBase64Tests.java
@@ -22,49 +22,77 @@ class BasicBase64Tests {
 
 	@Test
 	void testByteEncodingDecoding() {
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
 		byte[] inputBytes = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		byte[] encodedBytes = Base64Util.encodeByte(inputBytes);
-		byte[] decodedBytes = Base64Util.decodeBytes(encodedBytes);
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
 		assertArrayEquals(inputBytes, decodedBytes);
 	}
-
 	@Test
 	void testSimpleStringEncodingDecoding() {
-		String input = "Hello World!";
-		String encoded = Base64Util.encodeStr2Str(input);
-		String decoded = Base64Util.decodeStr2Str(encoded);
+		String input="Hello World!";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes();
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes);
 		assertEquals(input, decoded);
 	}
-
+	@Test
+	void testSimpleStringEncodingDecodingExplicitEncoding() {
+		String input="Hello World!";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes(Charset.forName("UTF-8"));
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes,Charset.forName("UTF-8"));
+		assertEquals(input, decoded);
+	}
 	@Test
 	void testSimpleStringEncodingDecodingNumeric() {
-		String input = "1234567890";
-		String encoded = Base64Util.encodeStr2Str(input);
-		String decoded = Base64Util.decodeStr2Str(encoded);
+		String input="1234567890";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes(Charset.forName("UTF-8"));
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes,Charset.forName("UTF-8"));
 		assertEquals(input, decoded);
 	}
-
 	@Test
 	void testSimpleStringEncodingDecodingLowercaseChars() {
-		String input = "abcdefghijklmnopqrstuvwxyz";
-		String encoded = Base64Util.encodeStr2Str(input);
-		String decoded = Base64Util.decodeStr2Str(encoded);
+		String input="abcdefghijklmnopqrstuvwxyz";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes(Charset.forName("UTF-8"));
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes,Charset.forName("UTF-8"));
 		assertEquals(input, decoded);
 	}
-
 	@Test
 	void testSimpleStringEncodingDecodingUppercaseChars() {
-		String input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		String encoded = Base64Util.encodeStr2Str(input);
-		String decoded = Base64Util.decodeStr2Str(encoded);
+		String input="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes(Charset.forName("UTF-8"));
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes,Charset.forName("UTF-8"));
 		assertEquals(input, decoded);
 	}
-
 	@Test
 	void testSimpleStringEncodingDecodingSpecialChars() {
-		String input = "!\"§$%&/()=?{[]}\\´`@+*~#'-_<>|,.";
-		String encoded = Base64Util.encodeStr2Str(input);
-		String decoded = Base64Util.decodeStr2Str(encoded);
+		String input="!\"§$%&/()=?{[]}\\´`@+*~#'-_<>|,.";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes(Charset.forName("UTF-8"));
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes,Charset.forName("UTF-8"));
 		assertEquals(input, decoded);
 	}
 }
