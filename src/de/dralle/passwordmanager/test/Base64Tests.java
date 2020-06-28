29e8bd9b7b39939782f59dc3667258f43695a67c
Nils Dralle <nils.91@googlemail.com>
2020-06-28 16:40:26 +0200

    test with explicit charset

--- src/de/dralle/passwordmanager/test/Base64Tests.java
+++ src/de/dralle/passwordmanager/test/Base64Tests.java
@@ -5,6 +5,7 @@ package de.dralle.passwordmanager.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.charset.Charset;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
@@ -34,8 +35,18 @@ class Base64Tests {
 		byte[] inputBytes = input.getBytes();
 		byte[] encodedBytes = encoder.encode(inputBytes);
 		byte[] decodedBytes = decoder.decode(encodedBytes);
-		String encoded=new String(decodedBytes);
-		assertEquals(input, encoded);
+		String decoded=new String(decodedBytes);
+		assertEquals(input, decoded);
 	}
-
+	@Test
+	void testSimpleStringEncodingDecodingExplicitEncoding() {
+		String input="Hello World!";
+		Encoder encoder = Base64.getEncoder();
+		Decoder decoder = Base64.getDecoder();
+		byte[] inputBytes = input.getBytes(Charset.forName("UTF-8"));
+		byte[] encodedBytes = encoder.encode(inputBytes);
+		byte[] decodedBytes = decoder.decode(encodedBytes);
+		String decoded=new String(decodedBytes,Charset.forName("UTF-8"));
+		assertEquals(input, decoded);
+	}
 }
