267ff0ec933b8557559c8ef61efe790e0dd14ebe
Nils Dralle <nils.91@googlemail.com>
2020-06-29 10:55:57 +0200

    basic hashing tests file

---
+++ src/de/dralle/passwordmanager/test/BasicHashingTests.java
@@ -0,0 +1,14 @@
+package de.dralle.passwordmanager.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BasicHashingTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
