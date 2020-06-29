af40b51dd60ab43259246e292d7fbb4fa9505895
Nils Dralle <nils.91@googlemail.com>
2020-06-29 10:53:30 +0200

    basic tests file

---
+++ src/de/dralle/passwordmanager/test/BasicSymmetricEncryptionTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.dralle.passwordmanager.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+class BasicSymmetricEncryptionTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
