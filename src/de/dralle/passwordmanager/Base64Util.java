9efb2fc9af4de985953d3af8d91dff5c1318eaa4
Nils Dralle <nils.91@googlemail.com>
2020-07-06 16:18:30 +0200

    base64util now uses stringutil

--- src/de/dralle/passwordmanager/Base64Util.java
+++ src/de/dralle/passwordmanager/Base64Util.java
@@ -15,7 +15,6 @@ import java.util.Base64.Encoder;
 public class Base64Util {
 	private static Encoder encoder=Base64.getEncoder();
 	private static Decoder decoder=Base64.getDecoder();
-	private static Charset cs=Charset.forName("UTF-8");
 	public static byte[] encodeByte(byte[] bytes) {
 		return encoder.encode(bytes);
 	}
@@ -23,28 +22,17 @@ public class Base64Util {
 		return decoder.decode(bytes);
 	}
 	
-	public static Charset getCharset() {
-		return cs;
-	}
-	public static String byteArrToStr(byte[] arr) {
-		return new String(arr, cs);
-	}
-	public static byte[] strToByteArr(String str) {
-		if(str==null) {
-			return null;
-		}
-		return str.getBytes(cs);
-	}
+	
 	public static byte[] encodeString(String str) {
-		return encodeByte(strToByteArr(str));
+		return encodeByte(StringUtil.strToByteArr(str));
 	}
 	public static byte[] decodeString(String str) {
-		return decodeBytes(strToByteArr(str));
+		return decodeBytes(StringUtil.strToByteArr(str));
 	}
 	public static String encodeStr2Str(String str) {
-		return byteArrToStr(encodeByte(strToByteArr(str)));
+		return StringUtil.byteArrToStr(encodeByte(StringUtil.strToByteArr(str)));
 	}
 	public static String decodeStr2Str(String str) {
-		return byteArrToStr(decodeBytes(strToByteArr(str)));
+		return StringUtil.byteArrToStr(decodeBytes(StringUtil.strToByteArr(str)));
 	}
 }
