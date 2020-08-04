63a3a21a28e7c17e40203e6f5718e6475f299272
Nils Dralle <nils.91@googlemail.com>
2020-08-04 15:06:56 +0200

    methods to hash strings

--- src/main/java/de/dralle/passwordmanager/HashingUtil.java
+++ src/main/java/de/dralle/passwordmanager/HashingUtil.java
@@ -12,27 +12,52 @@ import java.security.NoSuchAlgorithmException;
  */
 public class HashingUtil {
 	@Deprecated
-	public static final String ALGO_SHA_1="SHA-1";
-	
-	public static final String ALGO_SHA_256="SHA-256";
-	public static final String ALGO_SHA_512="SHA-512";
-	
-	public byte[] getHash(byte[] input,String algorithm) throws NoSuchAlgorithmException {
+	public static final String ALGO_SHA_1 = "SHA-1";
+
+	public static final String ALGO_SHA_256 = "SHA-256";
+	public static final String ALGO_SHA_512 = "SHA-512";
+
+	public byte[] getHash(String input, String algorithm) throws NoSuchAlgorithmException {
+		return getHash(StringUtil.strToByteArr(input), algorithm);
+
+	}
+
+	public byte[] getHash(byte[] input, String algorithm) throws NoSuchAlgorithmException {
 		MessageDigest sha = MessageDigest.getInstance(algorithm);
 		return sha.digest(input);
 	}
-	
+
 	public byte[] getHashSHA256(byte[] input) {
 		try {
 			return getHash(input, ALGO_SHA_256);
-		} catch (NoSuchAlgorithmException e) {			
+		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 		return null;
-	}public byte[] getHashSHA512(byte[] input) {
+	}
+
+	public byte[] getHashSHA512(byte[] input) {
 		try {
 			return getHash(input, ALGO_SHA_512);
-		} catch (NoSuchAlgorithmException e) {			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public byte[] getHashSHA256(String input) {
+		try {
+			return getHash(input, ALGO_SHA_256);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public byte[] getHashSHA512(String input) {
+		try {
+			return getHash(input, ALGO_SHA_512);
+		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 		return null;
