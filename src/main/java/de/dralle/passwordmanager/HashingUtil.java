5355d313fe19f03e4841134205f15ee45c003997
Nils Dralle <nils.91@googlemail.com>
2020-08-04 14:47:33 +0200

    added general hashing method to allow multiple algorithms

--- src/main/java/de/dralle/passwordmanager/HashingUtil.java
+++ src/main/java/de/dralle/passwordmanager/HashingUtil.java
@@ -11,14 +11,18 @@ import java.security.NoSuchAlgorithmException;
  *
  */
 public class HashingUtil {
+	
+	public byte[] getHash(byte[] input,String alghirthm) throws NoSuchAlgorithmException {
+		MessageDigest sha = MessageDigest.getInstance(alghirthm);
+		return sha.digest(input);
+	}
+	
 	public byte[] getHash(byte[] input) {
-		MessageDigest sha = null;
 		try {
-			sha = MessageDigest.getInstance("SHA-256");
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
+			return getHash(input, "SHA-256");
+		} catch (NoSuchAlgorithmException e) {			
 			e.printStackTrace();
 		}
-		return sha.digest(input);
+		return null;
 	}
 }
