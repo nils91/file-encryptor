99343b6129906d14b6829339b838ead2cd67d95a
Nils Dralle <nils.91@googlemail.com>
2020-08-04 14:59:50 +0200

    new test for stringutil

---
+++ src/test/java/de/dralle/passwordmanager/StringUtilTest.java
@@ -0,0 +1,49 @@
+package de.dralle.passwordmanager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringUtilTest {
+
+	@Test
+	void testSimpleStringEncodingDecoding() {
+		String input = "Hello World!";
+		byte[] bytes = StringUtil.strToByteArr(input);
+		String dStr = StringUtil.byteArrToStr(bytes);
+		assertEquals(input, dStr);
+	}
+
+	@Test
+	void testSimpleStringEncodingDecodingNumeric() {
+		String input = "1234567890";
+		byte[] bytes = StringUtil.strToByteArr(input);
+		String dStr = StringUtil.byteArrToStr(bytes);
+		assertEquals(input, dStr);
+	}
+
+	@Test
+	void testSimpleStringEncodingDecodingLowercaseChars() {
+		String input = "abcdefghijklmnopqrstuvwxyz";
+		byte[] bytes = StringUtil.strToByteArr(input);
+		String dStr = StringUtil.byteArrToStr(bytes);
+		assertEquals(input, dStr);
+	}
+
+	@Test
+	void testSimpleStringEncodingDecodingUppercaseChars() {
+		String input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		byte[] bytes = StringUtil.strToByteArr(input);
+		String dStr = StringUtil.byteArrToStr(bytes);
+		assertEquals(input, dStr);
+	}
+
+	@Test
+	void testSimpleStringEncodingDecodingSpecialChars() {
+		String input = "!\"§$%&/()=?{[]}\\´`@+*~#'-_<>|,.";
+		byte[] bytes = StringUtil.strToByteArr(input);
+		String dStr = StringUtil.byteArrToStr(bytes);
+		assertEquals(input, dStr);
+	}
+
+}
