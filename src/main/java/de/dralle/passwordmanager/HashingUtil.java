e2c3853015b788bcae10e2f3d36ae54b0aa3677b
Nils Dralle <nils.91@googlemail.com>
2020-08-05 14:12:23 +0200

    methods for string

--- src/main/java/de/dralle/passwordmanager/HashingUtil.java
+++ src/main/java/de/dralle/passwordmanager/HashingUtil.java
@@ -115,8 +115,11 @@ public class HashingUtil {
 		byte[] hash = getHash(saltedInput, algorithm);
 		byte[] saltedHash = concatenateArrays(salt, hash);
 		return saltedHash;
-	}/**
+	}
+
+	/**
 	 * The first byte of the returned salted hash is the length of the salt.
+	 * 
 	 * @param input
 	 * @param salt      Salt will be generated randomly and be written into the
 	 *                  given array. It will also be added to the front of the
@@ -127,26 +130,29 @@ public class HashingUtil {
 	 * @return Salted hash. The salt will be added in front of the hash.
 	 * @throws NoSuchAlgorithmException
 	 */
-	public byte[] getSaltedHashIncludingSaltLen(byte[] input, byte[] salt, String algorithm) throws NoSuchAlgorithmException {
+	public byte[] getSaltedHashIncludingSaltLen(byte[] input, byte[] salt, String algorithm)
+			throws NoSuchAlgorithmException {
 		if (salt == null) {
 			return getHash(input, algorithm);
 		}
-		int saltLen=salt.length;
+		int saltLen = salt.length;
 		byte[] saltedHash = getSaltedHash(input, salt, algorithm);
-		byte[] saltedHashWSaltLen = concatenateArrays(new byte[] {(byte) saltLen}, saltedHash);
+		byte[] saltedHashWSaltLen = concatenateArrays(new byte[] { (byte) saltLen }, saltedHash);
 		return saltedHashWSaltLen;
 	}
-	public byte[] concatenateArrays(byte[] arr1,byte[] arr2) {
-		byte[] newArr=new byte[arr1.length+arr2.length];
+
+	public byte[] concatenateArrays(byte[] arr1, byte[] arr2) {
+		byte[] newArr = new byte[arr1.length + arr2.length];
 		for (int i = 0; i < newArr.length; i++) {
-			if(i<arr1.length) {
-				newArr[i]=arr1[i];
-			}else {
-				newArr[i]=arr2[i-arr1.length];
+			if (i < arr1.length) {
+				newArr[i] = arr1[i];
+			} else {
+				newArr[i] = arr2[i - arr1.length];
 			}
 		}
 		return newArr;
 	}
+
 	/**
 	 * 
 	 * @param input
@@ -179,21 +185,96 @@ public class HashingUtil {
 		}
 		return checkSaltedHash(input, salt, hash, algorithm);
 	}
+
 	/**
-	 * If no salt length is given, it is assumed that the first byte of saltedHash is the salt length.
+	 * If no salt length is given, it is assumed that the first byte of saltedHash
+	 * is the salt length.
+	 * 
 	 * @param input
 	 * @param saltedHash
 	 * @param algorithm
 	 * @return
 	 * @throws NoSuchAlgorithmException
 	 */
-	public boolean checkSaltedHash(byte[] input, byte[] saltedHash, String algorithm)
-			throws NoSuchAlgorithmException {
-		int saltLen=saltedHash[0];
-		byte[] saltedHashWOSaltLen = new byte[saltedHash.length-1];
+	public boolean checkSaltedHash(byte[] input, byte[] saltedHash, String algorithm) throws NoSuchAlgorithmException {
+		int saltLen = saltedHash[0];
+		byte[] saltedHashWOSaltLen = new byte[saltedHash.length - 1];
 		for (int i = 0; i < saltedHashWOSaltLen.length; i++) {
-			saltedHashWOSaltLen[i]=saltedHash[i+1];
+			saltedHashWOSaltLen[i] = saltedHash[i + 1];
 		}
 		return checkSaltedHash(input, saltLen, saltedHashWOSaltLen, algorithm);
+	}
+
+	/**
+	 * 
+	 * @param input
+	 * @param salt      Salt will be generated randomly and be written into the
+	 *                  given array. It will also be added to the front of the
+	 *                  returned hash. Salt length is determined by the size of this
+	 *                  array. If salt is null, just the hash of input will be
+	 *                  returned.
+	 * @param algorithm
+	 * @return Salted hash. The salt will be added in front of the hash.
+	 * @throws NoSuchAlgorithmException
+	 */
+	public byte[] getSaltedHash(String input, byte[] salt, String algorithm) throws NoSuchAlgorithmException {
+		byte[] inputArr = StringUtil.strToByteArr(input);
+		return getSaltedHash(inputArr, salt, algorithm);
+	}
+
+	/**
+	 * The first byte of the returned salted hash is the length of the salt.
+	 * 
+	 * @param input
+	 * @param salt      Salt will be generated randomly and be written into the
+	 *                  given array. It will also be added to the front of the
+	 *                  returned hash. Salt length is determined by the size of this
+	 *                  array. If salt is null, just the hash of input will be
+	 *                  returned.
+	 * @param algorithm
+	 * @return Salted hash. The salt will be added in front of the hash.
+	 * @throws NoSuchAlgorithmException
+	 */
+	public byte[] getSaltedHashIncludingSaltLen(String input, byte[] salt, String algorithm)
+			throws NoSuchAlgorithmException {
+		byte[] inputArr = StringUtil.strToByteArr(input);
+		return getSaltedHashIncludingSaltLen(inputArr, salt, algorithm);
+	}
+
+	/**
+	 * 
+	 * @param input
+	 * @param salt      Byte array to be combined with input as the salt before
+	 *                  hashing.
+	 * @param hash      Not salted hash.
+	 * @param algorithm
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public boolean checkSaltedHash(String input, byte[] salt, byte[] hash, String algorithm)
+			throws NoSuchAlgorithmException {
+		byte[] inputArr = StringUtil.strToByteArr(input);
+		return checkSaltedHash(inputArr, salt, hash, algorithm);
+	}
+
+	public boolean checkSaltedHash(String input, int saltLen, byte[] saltedHash, String algorithm)
+			throws NoSuchAlgorithmException {
+		byte[] inputArr = StringUtil.strToByteArr(input);
+		return checkSaltedHash(inputArr, saltLen, saltedHash, algorithm);
+	}
+
+	/**
+	 * If no salt length is given, it is assumed that the first byte of saltedHash
+	 * is the salt length.
+	 * 
+	 * @param input
+	 * @param saltedHash
+	 * @param algorithm
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public boolean checkSaltedHash(String input, byte[] saltedHash, String algorithm) throws NoSuchAlgorithmException {
+		byte[] inputArr = StringUtil.strToByteArr(input);
+		return checkSaltedHash(inputArr, saltedHash, algorithm);
 	}
 }
