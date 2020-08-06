c1bca9a4aa0ec99feaf418f05ff5fed106ea3c30
Nils Dralle <nils.91@googlemail.com>
2020-08-06 14:14:00 +0200

    new basic encryption tests

--- src/test/java/de/dralle/passwordmanager/test/BasicSymmetricEncryptionTests.java
+++ src/test/java/de/dralle/passwordmanager/test/BasicSymmetricEncryptionTests.java
@@ -5,6 +5,10 @@ package de.dralle.passwordmanager.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +22,16 @@ class BasicSymmetricEncryptionTests {
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	@Test
+	void testAESAvailability(){
+		KeyGenerator keygen=null;
+		try {
+			keygen=KeyGenerator.getInstance("AES");
+		} catch (NoSuchAlgorithmException e) {
+			fail();
+		}
+		assertNotNull(keygen);
 	}
 
 }
