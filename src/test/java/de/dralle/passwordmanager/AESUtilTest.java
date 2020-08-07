8d0971c9918afed137ef0da07db5b71cef320a43
Nils Dralle <nils.91@googlemail.com>
2020-08-07 19:53:47 +0200

    AES Util tests

---
+++ src/test/java/de/dralle/passwordmanager/AESUtilTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.dralle.passwordmanager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+class AESUtilTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
