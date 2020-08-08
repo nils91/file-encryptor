9f07e9976dafe69151ac85b6f47e0fad75655c82
Nils Dralle <nils.91@googlemail.com>
2020-08-08 19:11:29 +0200

    disabled non working cli options

--- src/main/java/de/dralle/fileenc/FileEncryptorCLIApp.java
+++ src/main/java/de/dralle/fileenc/FileEncryptorCLIApp.java
@@ -213,20 +213,20 @@ public class FileEncryptorCLIApp {
 	private Options prepareOptions() {
 		Options options = new Options();
 		options.addOption("e", "encrypt", false, "This flag sets the application to encryption mode");
-		options.addOption("d", "decrypt", false, "This flag sets the application to decryption mode");
+		//options.addOption("d", "decrypt", false, "This flag sets the application to decryption mode");
 		options.addOption("i", "in", true, "Input file");
 		options.addOption("o", "out", true,
 				"Output file name. If this parameter is not given, the output filename in encryption mode is just the input filename + '.enc' or '.dec' in decryption mode.");
-		options.addOption("r", "remove", false, "Remove the input file after the operation is complete");
+		//options.addOption("r", "remove", false, "Remove the input file after the operation is complete");
 		options.addOption("t", "verbose", false, "Switch on verbode mode");
-		options.addOption("c", "verify", false, "Verify the operation");
-		options.addOption("p", "password", true, "Encrypt/Decrypt using a password");
-		options.addOption("k", "key", true, "Encrypt/Decrypt using a key. Must be encoded in Base64");
+		//options.addOption("c", "verify", false, "Verify the operation");
+		//options.addOption("p", "password", true, "Encrypt/Decrypt using a password");
+		//options.addOption("k", "key", true, "Encrypt/Decrypt using a key. Must be encoded in Base64");
 		options.addOption("w", "writekey", false, "Write the key to the console once operation is complete");
 		options.addOption("h", "help", false, "Show usage instructions");
 		options.addOption("v", "version", false, "Show version information");
-		options.addOption("g", "gui", false, "Show GUI");
-		options.addOption("b", "blocks", true, "Encrypt the file in block instead of as a whole");
+		//options.addOption("g", "gui", false, "Show GUI");
+		//options.addOption("b", "blocks", true, "Encrypt the file in block instead of as a whole");
 		return options;
 	}
 
