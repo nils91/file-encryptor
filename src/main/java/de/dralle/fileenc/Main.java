96ebcece393fd64ea9de5ca951008162d51dbdeb
Nils Dralle <nils.91@googlemail.com>
2020-08-08 15:09:45 +0200

    More command line options

--- src/main/java/de/dralle/fileenc/Main.java
+++ src/main/java/de/dralle/fileenc/Main.java
@@ -28,22 +28,26 @@ public class Main {
 		options.addOption("o", "out", true,
 				"Output file name. If this parameter is not given, the output filename in encryption mode is just the input filename + '.enc' or '.dec' in decryption mode.");
 		options.addOption("r", "remove", false, "Remove the input file after the operation is complete");
-		options.addOption("v", "verbose", false, "Switch on verbode mode");
+		options.addOption("t", "verbose", false, "Switch on verbode mode");
 		options.addOption("c", "verify", false, "Verify the operation");
 		options.addOption("p", "password", true, "Encrypt/Decrypt using a password");
 		options.addOption("k", "key", true, "Encrypt/Decrypt using a key. Must be encoded in Base64");
 		options.addOption("w", "writekey", false, "Write the key to the console once operation is complete");
 		options.addOption("h", "help", false, "Show usage instructions");
+		options.addOption("v", "version", false, "Show version information");
+		options.addOption("g","gui",false,"Show GUI");
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = null;
 		try {
 			cmd = parser.parse(options, args);
 		} catch (ParseException e) {
-			System.out.println("Arguments coul not be parser. Use '-h' or '--help' to show correct usage.");
+			System.out.println("Arguments could not be parser. Use '-h' or '--help' to show correct usage.");
+			System.exit(1);
 		}
 		if (cmd.hasOption("h")) {
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("File Encryptor", options);
+			System.exit(0);
 		}
 	}
 
