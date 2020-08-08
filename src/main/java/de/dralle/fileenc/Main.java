02b5eb85a653eab1276d5494df5ee730188eb1a2
Nils Dralle <nils.91@googlemail.com>
2020-08-08 14:02:17 +0200

    Main.java file

---
+++ src/main/java/de/dralle/fileenc/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.dralle.fileenc;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
