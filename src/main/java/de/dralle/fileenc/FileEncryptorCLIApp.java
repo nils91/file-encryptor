83e6bbf9f56e35178534194cc963c5bdd402b201
Nils Dralle <nils.91@googlemail.com>
2020-08-09 14:20:42 +0200

    write key file

--- src/main/java/de/dralle/fileenc/FileEncryptorCLIApp.java
+++ src/main/java/de/dralle/fileenc/FileEncryptorCLIApp.java
@@ -350,7 +350,61 @@ public class FileEncryptorCLIApp {
 			System.out.println(completeFileEncryptedWithIVPrefix.length + " Bytes written");
 		}
 		if (cmd.hasOption("w")) {
-			System.out.println(Base64Util.encodeBytes2Str(key.getEncoded()));
+			String writeKeyFile=cmd.getOptionValue("w");	
+			File tgtKeyFile=null;
+			if(writeKeyFile==null) {
+				if(verbose) {
+					System.out.println("Parameter '-w' has no argument. Writing key to console.");
+				}
+				System.out.println(Base64Util.encodeBytes2Str(key.getEncoded()));
+			}else if(writeKeyFile.equalsIgnoreCase("yes")) {
+				tgtKeyFile=new File(outputFile.getAbsoluteFile()+".key");
+				if(verbose) {
+					System.out.println(String.format("Parameter '-w' has argument '%s'. Writing key to filename generated from output filename.", writeKeyFile));
+				}
+			}else {
+				tgtKeyFile=new File(writeKeyFile);
+				if(verbose) {
+					System.out.println(String.format("Parameter '-w' has argument '%s'. Trying to write key there.", writeKeyFile));
+				}
+			}
+			if(tgtKeyFile!=null) {
+				if(verbose) {
+					System.out.println(String.format("Writing key to %s.", tgtKeyFile.getAbsolutePath()));
+				}
+				// Write key
+				OutputStream keyout = null;
+				try {
+					keyout = new FileOutputStream(tgtKeyFile);
+				} catch (FileNotFoundException e) {
+					System.out.println("Error while opening key file");
+					if (verbose) {
+						e.printStackTrace();
+					}
+					System.exit(1);
+				}
+				BufferedOutputStream keybout = new BufferedOutputStream(keyout);
+				try {
+					keybout.write(key.getEncoded());
+				} catch (IOException e) {
+					System.out.println("Error while writing key file");
+					if (verbose) {
+						e.printStackTrace();
+					}
+					System.exit(1);
+				}
+				try {
+					keybout.close();
+				} catch (IOException e) {
+					System.out.println("Error while closing key file");
+					if (verbose) {
+						e.printStackTrace();
+					}
+				}
+				if(verbose) {
+					System.out.println("Key written to "+tgtKeyFile.getAbsolutePath());
+				}
+			}
 		}
 
 	}
@@ -381,7 +435,7 @@ public class FileEncryptorCLIApp {
 	private Options prepareOptions() {
 		Options options = new Options();
 		options.addOption("e", "encrypt", false, "This flag sets the application to encryption mode");
-		 options.addOption("d", "decrypt", false, "This flag sets the application to decryption mode");
+		options.addOption("d", "decrypt", false, "This flag sets the application to decryption mode");
 		options.addOption("i", "in", true, "Input file");
 		options.addOption("o", "out", true,
 				"Output file name. If this parameter is not given, the output filename in encryption mode is just the input filename + '.enc' or '.dec' in decryption mode.");
@@ -390,7 +444,7 @@ public class FileEncryptorCLIApp {
 		options.addOption("t", "verbose", false, "Switch on verbode mode");
 		// options.addOption("c", "verify", false, "Verify the operation");
 		// options.addOption("p", "password", true, "Encrypt/Decrypt using a password");
-		options.addOption("k", "key", true, "Encrypt/Decrypt using a key. Must be encoded in Base64");
+		options.addOption("k", "key",true, "Encrypt/Decrypt using a key. Must be encoded in Base64");
 		options.addOption("w", "writekey", false, "Write the key to the console once operation is complete");
 		options.addOption("h", "help", false, "Show usage instructions");
 		options.addOption("v", "version", false, "Show version information");
