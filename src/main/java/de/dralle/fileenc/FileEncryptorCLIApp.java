638f20328a897592dbed54a033a1060732468270
Nils Dralle <nils.91@googlemail.com>
2020-08-09 18:59:08 +0200

    Constructor and printstream

--- src/main/java/de/dralle/fileenc/FileEncryptorCLIApp.java
+++ src/main/java/de/dralle/fileenc/FileEncryptorCLIApp.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +39,10 @@ import de.dralle.util.Base64Util;
 public class FileEncryptorCLIApp {
 	public final static String VERSION = "0.0.2";
 	private boolean verbose;
+	private PrintStream out;
+	public FileEncryptorCLIApp() {
+		out=System.out;
+	}
 
 	public void run(String[] args) {
 		Options options = prepareOptions();
