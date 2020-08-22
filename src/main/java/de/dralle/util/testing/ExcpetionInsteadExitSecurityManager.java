b13907a0f48af1e9107a4d93eb975ebbfbcf6f4a
Nils Dralle <nils.91@googlemail.com>
2020-08-22 16:10:22 +0200

    no exit security manager

--- src/main/java/de/dralle/util/testing/ExcpetionInsteadExitSecurityManager.java
+++ src/main/java/de/dralle/util/testing/ExcpetionInsteadExitSecurityManager.java
@@ -3,10 +3,27 @@
  */
 package de.dralle.util.testing;
 
+import java.security.Permission;
+
 /**
  * @author Nils Dralle
  *
  */
-public class ExcpetionInsteadExitSecurityManager extends SecurityManager{
+public class ExcpetionInsteadExitSecurityManager extends SecurityManager {
+
+	@Override
+	public void checkPermission(Permission perm) {// allow anything
+	}
+
+	@Override
+	public void checkPermission(Permission perm, Object context) {// allow anything
+	}
+
+	@Override
+	public void checkExit(int status) {
+		// TODO Auto-generated method stub
+		super.checkExit(status);
+		throw new ExitException(status);
+	}
 
 }
