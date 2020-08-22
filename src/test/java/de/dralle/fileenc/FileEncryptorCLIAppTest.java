c96bea8a249c7e39cdfa6a3eb704b59dae624f2c
Nils Dralle <nils.91@googlemail.com>
2020-08-22 12:43:54 +0200

    full systems test junit test start

---
+++ src/test/java/de/dralle/fileenc/FileEncryptorCLIAppTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.dralle.fileenc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+class FileEncryptorCLIAppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
